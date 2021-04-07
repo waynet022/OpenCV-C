1b61f1125b734fb62c03e21e4b6394efa3cb9288
wayne1tam <waynevick7226@hotmail.com>
2021-04-07 17:45:14 -0400

    Read image

---
+++ opencv/opencv/read.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void main() {
+	string path = "Resources/test.png";
+	Mat img = imread(path);
+	imshow("Image", img);
+	waitKey(0);
+}
